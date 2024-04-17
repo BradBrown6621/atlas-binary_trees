3f6c73c0dff9ab1c1cf5c4e08a8dfc8b28a2c506
BradBrown6621 <6621@holbertonstudents.com>
2024-04-17 12:06:17 -0500

    1-: Added check that makes sure 'parent' exists/isn't NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,6 +4,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
 
+	if (!parent)
+	{
+		return (NULL);
+	}
+
 	new = binary_tree_node(parent, value);
 
 	if (parent->left)
