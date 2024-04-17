b5522704252ea636f49891d6c5e1a81554addb74
BradBrown6621 <6621@holbertonstudents.com>
2024-04-17 17:40:44 -0500

    Fixed backwards logic for comparing left vs right height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -32,7 +32,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left = binary_tree_height(tree->left) + 1;
 	right = binary_tree_height(tree->right) + 1;
 
-	if (left < right)
+	if (left > right)
 	{
 		return (left);
 	}
