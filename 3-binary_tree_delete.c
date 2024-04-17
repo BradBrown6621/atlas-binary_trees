2583474d7c56f88c22cfd7621908bf9a60a4a832
BradBrown6621 <6621@holbertonstudents.com>
2024-04-17 15:47:29 -0500

    Betty documentation added to 3-

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,17 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - Free's the memory for a binary_tree_t
+ *
+ * @tree: The tree to be deleted
+ *
+ * Description:
+ * [TRAVERSAL]
+ * - This algorithm uses post-order traversal to traverse a binary tree
+ *
+ * Return: NOTHING.
+ */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (!tree)
