2847e8b981f0f1d42df5556c741df44cf0a651e0
BradBrown6621 <6621@holbertonstudents.com>
2024-04-18 10:14:08 -0500

    Added 14- and updated corresponding header file (binary_trees)

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int balance = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (tree->left)
+	{
+		balance++;
+	}
+	if (tree->right)
+	{
+		balance--;
+	}
+
+	balance = balance + binary_tree_balance(tree->left) -
+		binary_tree_balance(tree->right);
+
+	printf("%i balance: %i\n", tree->n, balance);
+
+	return (balance);
+}
