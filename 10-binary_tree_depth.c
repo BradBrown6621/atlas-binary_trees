e0487c0e4d7b278e27e2011cce1206ea19bef11a
BradBrown6621 <6621@holbertonstudents.com>
2024-04-17 22:26:28 -0500

    Betty documentation for 10-

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - Finds the depth of a given node ("tree")
+ *
+ * @tree: The node to find the depth of in its tree
+ *
+ * Return: The depth of the node, or 0 if "tree" is "NULL"
+ */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	const binary_tree_t *current;
