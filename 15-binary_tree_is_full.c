73f41e1ea3c38c6f47382041ec1f1eeb67f2da89
BradBrown6621 <6621@holbertonstudents.com>
2024-04-18 23:10:35 -0500

    15-: Added Betty documentation and updated styling

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,5 +1,20 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_full - Check if a binary tree is considered "full"
+ *
+ * @tree: The tree to be evaluated
+ *
+ * Description:
+ * ["FULL"]
+ * A binary tree is considered "full" if every node has either
+ * 0 or 2 children. Because each subtree is a tree, any subtree in a "full"
+ * tree is itself considered a "full" tree, for if any subtree is
+ * itself not "full", then the whole tree it is in is not "full".
+ *
+ * Return: 1 if the tree is full, 0 if it is not full or if the tree is "NULL"
+ */
+
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
@@ -12,8 +27,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 	} else if (!tree->left || !tree->right)
 	{
-		/** logically, if we have entered this part of the function,
-		 * at least 1 child should be missing.
+		/**
+		 * logically, if we have entered this part of the function,
+		 * at least 1 child should be missing, therefore making this
+		 * subtree not a "full" tree.
 		 */
 		return (0);
 	}
