fba08b121e8d955deef0140c7a10a64bca8f04e2
BradBrown6621 <6621@holbertonstudents.com>
2024-04-19 00:44:49 -0500

    16-: Added recursion to deal with full trees with heights greater than 3

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -142,6 +142,15 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 
+	if (tree->left && binary_tree_is_perfect(tree->left) == 0)
+	{
+		return (0);
+	}
+	if (tree->right && binary_tree_is_perfect(tree->right) == 0)
+	{
+		return (0);
+	}
+
 	if (binary_tree_balance(tree) != 0)
 	{
 		return (0);
@@ -151,5 +160,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (1);
 	}
-	return (0);
+	return (1);
 }
